da54a43e9dbf809a1b2d1a299571d7bc96c9694f
lose_sea <2031938936@qq.com>
2026-01-12 21:38:14 +0800

    test

---
+++ 变量/变量/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  变量
+//
+//  Created by lose_sea on 2026/1/12.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+        int i = 100;
+        NSLog(@"i: %d", i); 
+    }
+    return EXIT_SUCCESS;
+}
---
+++ 变量/变量/person.h
@@ -0,0 +1,7 @@
+//
+//  person.h
+//  变量
+//
+//  Created by lose_sea on 2026/1/12.
+//
+
