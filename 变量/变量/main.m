1fae2e2c6afe1fb061833cbaef2859b6cc008551
lose_sea <2031938936@qq.com>
2026-01-12 22:47:58 +0800

    test

--- 变量/变量/main.m
+++ 变量/变量/main.m
@@ -5,14 +5,42 @@
 //  Created by lose_sea on 2026/1/12.
 //
 
-#import <Foundation/Foundation.h>
+//#import <Foundation/Foundation.h>
+////#import"person.h"
+//int main(int argc, const char * argv[]) {
+//    @autoreleasepool {
+//        // insert code here...
+//        NSLog(@"Hello, World!");
+//        int i = 100;
+//        NSLog(@"i: %d", i); 
+//    }
+//    // age = 20;
+//    for (int i = 0; i < 10; i++) {
+//        NSLog (@"i val: %d", i);
+//    }
+//    return EXIT_SUCCESS;
+//}
 
+
+
+//#import <Foundation/Foundation.h>
+////#import"person.h"
+//int main(int argc, const char * argv[]) {
+//    @autoreleasepool {
+//        for (int i = 0; i < 10; i++) {
+//            NSLog (@"i val: %d", i);
+//        }
+//    }
+//    return EXIT_SUCCESS;
+//}
+
+#import <Foundation/Foundation.h>
+//#import"person.h"
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
-        int i = 100;
-        NSLog(@"i: %d", i); 
+        int i = 0;
+        scanf("%d", &i);
+        NSLog(@"%d", i);
     }
     return EXIT_SUCCESS;
 }
--- 变量/变量/person.h
+++ 变量/变量/person.h
@@ -5,3 +5,11 @@
 //  Created by lose_sea on 2026/1/12.
 //
 
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject {
+    // 实例变量
+    int age;
+    int sex;
+}
+@end
