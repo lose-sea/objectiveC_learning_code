e8f33d1b035ccff78c7bedd1d1c380f92c11090b
lose_sea <2031938936@qq.com>
2026-01-16 22:10:25 +0800

    test

--- 变量/变量/main.m
+++ 变量/变量/main.m
@@ -63,4 +63,32 @@
 //}
 
 
+//#import<Foundation/Foundation.h>
+//int main(int argc, char* argv[]) {
+//    @autoreleasepool {
+//        int arr[5];
+//        for (int i = 0, lenth = sizeof(arr) / sizeof(arr[0]); i < lenth; i++) {
+//            NSLog(@"arr[%d]: %d", i, arr[i]);
+//        }
+//    }
+//    return EXIT_SUCCESS;
+//}
 
+
+#import<Foundation/Foundation.h>
+int main(int argc, char* argv[]) {
+    @autoreleasepool {
+        int arr[3][4] = {
+            {1, 2, 3, 4},
+            {5, 6, 6, 7},
+            {8, 8, 4, 5}
+        };
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 4; j++) {
+                printf("%d\t", arr[i][j]);
+            }
+            printf("\n");
+        }
+    }
+    return EXIT_SUCCESS;
+}
