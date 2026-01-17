4e296110552e87734e5e687326775ac3184caf16
lose_sea <2031938936@qq.com>
2026-01-17 19:32:57 +0800

    test

--- 变量/变量/main.m
+++ 变量/变量/main.m
@@ -75,20 +75,49 @@
 //}
 
 
+//#import<Foundation/Foundation.h>
+//int main(int argc, char* argv[]) {
+//    @autoreleasepool {
+//        int arr[3][4] = {
+//            {1, 2, 3, 4},
+//            {5, 6, 6, 7},
+//            {8, 8, 4, 5}
+//        };
+//        for (int i = 0; i < 3; i++) {
+//            for (int j = 0; j < 4; j++) {
+//                printf("%d\t", arr[i][j]);
+//            }
+//            printf("\n");
+//        }
+//    }
+//    return EXIT_SUCCESS;
+//}
+ 
+
 #import<Foundation/Foundation.h>
+@interface DataWrap : NSObject  // 定义一个DataWrap类
+// 为DataWrap定义a, b 两个属性
+@property int a;
+@property int b;
+@end
+@implementation DataWrap
+@synthesize a, b;   //为a, b两个属性合成访问器方法和实例变量
+@end
+void swap(DataWrap* dw) {
+    // 下面这三行代码实现 dw 中的 a , b 两个属性的值交换
+    int tem = dw.a;
+    dw.a = dw.b;
+    dw.b = tem;
+    NSLog(@"swap函数里, 属性a的值是 : %d, 属性b的值是: %d", dw.a, dw.b);
+}
 int main(int argc, char* argv[]) {
     @autoreleasepool {
-        int arr[3][4] = {
-            {1, 2, 3, 4},
-            {5, 6, 6, 7},
-            {8, 8, 4, 5}
-        };
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 4; j++) {
-                printf("%d\t", arr[i][j]);
-            }
-            printf("\n");
-        }
+        DataWrap* dw = [[DataWrap alloc] init];
+        dw.a = 6;
+        dw.b = 3;
+        swap(dw);
+        NSLog(@"交换结束后, 属性 a 的值是: %d, 属性b的值是 %d", dw.a, dw.b);
     }
-    return EXIT_SUCCESS;
 }
+    
+
