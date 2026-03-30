ad5a98ad922f9441435174f4d07b94e4703e18eb
lose_sea <2031938936@qq.com>
2026-03-30 17:14:58 +0800

    test

--- test 方法重写/test 方法重写/User.m
+++ test 方法重写/test 方法重写/User.m
@@ -1,60 +1,109 @@
+////
+////  User.m
+////  test 方法重写
+////
+////  Created by lose_sea on 2026/3/21.
+////
 //
-//  User.m
-//  test 方法重写
+//#import "User.h"
 //
-//  Created by lose_sea on 2026/3/21.
+//@implementation User
+//// 单例模式
+//static User* _instance = nil;
+//+ (instancetype) shareInstance {
+//    static dispatch_once_t tokenOnce;
+//    dispatch_once(&tokenOnce, ^ {
+//        _instance = [[super allocWithZone: NULL] init];
+//    });
+//    return _instance;
+//}
+//+ (instancetype) allocWithzone: (struct _NSZone*) zone {
+//    return [self shareInstance];
+//}
+//- (id) copyWithZone: (NSZone*) zone {
+//    return self;
+//}
+//- (id) mutableCopyWithZone: (NSZone*) zone {
+//    return self;
+//}
 //
+//
+//
+//// 重写 isEqual:方法
+//- (BOOL) isEqual: (id) other {
+//    if (self == other) {
+//        return YES;
+//    }
+//    if (other != nil && [other isMemberOfClass: self.class]) {
+//        User* target = (User*) other;
+//        return ([self.name isEqual: target.name] && [self.pass isEqual: target.pass]);
+//    }
+//    return NO;
+//}
+//
+//
+//
+//// 重写hash 方法
+//- (NSUInteger) hash {
+//    NSUInteger nameHash = _name == nil ? 0 : [_name hash];
+//    NSUInteger passHash = _pass == nil ? 0 : [_pass hash];
+//    return nameHash * 31 + passHash;
+//}
+//
+//- (void) jump {
+//    NSLog(@"jumping");
+//}
+//- (void) run {
+//    [self jump];
+//    NSLog(@"running");
+//}
+//@end
 
-#import "User.h"
+
+#import"User.h"
+
 
 @implementation User
-// 单例模式
-static User* _instance = nil;
-+ (instancetype) shareInstance {
-    static dispatch_once_t tokenOnce;
-    dispatch_once(&tokenOnce, ^ {
+static User* _instance;
+//- (BOOL) isEqual: (id) other {
+//    if (self == other) {
+//        return YES;
+//    }
+//    if (other != nil && [other isMemberOfClass: self.class]) {
+//        User* target = (User*)other;
+//        return [target isEqual: self.name] && [target.pass isEqual: self.pass];
+//    }
+//    return NO;
+//}
+- (NSUInteger)hash {
+    NSUInteger nameHash = _name == nil ? 0 : [self.name hash];
+    NSUInteger passHash = _pass == nil ? 0 : [self.pass hash];
+    return nameHash * 31 + passHash;
+}
+
+- (void)run {
+}
+
++ (instancetype)shareInstance {
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
         _instance = [[super allocWithZone: NULL] init];
     });
     return _instance;
 }
-+ (instancetype) allocWithzone: (struct _NSZone*) zone {
-    return [self shareInstance];
-}
-- (id) copyWithZone: (NSZone*) zone {
-    return self;
-}
-- (id) mutableCopyWithZone: (NSZone*) zone {
-    return self;
-}
 
-
-
-// 重写 isEqual:方法
-- (BOOL) isEqual: (id) other {
+- (BOOL) isEqual: (id)other {
     if (self == other) {
         return YES;
     }
     if (other != nil && [other isMemberOfClass: self.class]) {
-        User* target = (User*) other;
-        return ([self.name isEqual: target.name] && [self.pass isEqual: target.pass]);
+        User* target = (User*)other;
+        return [self.name isEqual: target.name] && [self.pass isEqual: target.pass];
     }
     return NO;
 }
 
-
-
-// 重写hash 方法
-- (NSUInteger) hash {
-    NSUInteger nameHash = _name == nil ? 0 : [_name hash];
-    NSUInteger passHash = _pass == nil ? 0 : [_pass hash];
-    return nameHash * 31 + passHash;
+- (void)jump {
 }
 
-- (void) jump {
-    NSLog(@"jumping");
-}
-- (void) run {
-    [self jump];
-    NSLog(@"running"); 
-}
 @end
