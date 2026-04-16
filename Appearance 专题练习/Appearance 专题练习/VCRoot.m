c81c026d22c7a40fe3de32c1aac3a959195ffe22
lose_sea <2031938936@qq.com>
2026-04-17 00:02:18 +0800

    test

--- Appearance 专题练习/Appearance 专题练习/VCRoot.m
+++ Appearance 专题练习/Appearance 专题练习/VCRoot.m
@@ -12,77 +12,126 @@
 @end
 
 @implementation VCRoot
-- (void) test01 {
+//- (void) test01 {
+//    self.view.backgroundColor = [UIColor systemCyanColor];
+//    self.title = @"VCRoot";
+//    
+//    UIBarButtonItem* leftBtn = [[UIBarButtonItem alloc] initWithTitle: @"Btn" style: UIBarButtonItemStylePlain target: self action: @selector(pressTap)];
+//    self.navigationItem.leftBarButtonItem = leftBtn;
+//    
+//    // 设置不透明
+//    // 默认是 YES, 透明
+//    self.navigationController.navigationBar.translucent = YES;
+//    
+//    // 设置导航栏的背景颜色
+//    self.navigationController.navigationBar.barTintColor = [UIColor redColor];
+//    
+//    UINavigationBarAppearance* appearance = [[UINavigationBarAppearance alloc] init];
+////    [appearance configureWithOpaqueBackground];
+//    
+//    appearance.backgroundColor = [UIColor greenColor];
+//    
+//    // 标题颜色
+//    appearance.titleTextAttributes = @{
+//        NSForegroundColorAttributeName: [UIColor redColor]
+//    };
+//    
+////    // 按钮颜色(按钮的文字颜色)
+////    appearance.buttonAppearance.normal.titleTextAttributes = @{
+////        NSForegroundColorAttributeName: [UIColor blueColor]
+////    };
+//    
+//    appearance.buttonAppearance.normal.titleTextAttributes = @{
+//        NSForegroundColorAttributeName: [UIColor blackColor]
+//    };
+//    
+//    UINavigationBar* navbar = self.navigationController.navigationBar;
+//    navbar.standardAppearance = appearance;
+//    navbar.scrollEdgeAppearance = appearance;
+//    
+//    
+//    
+//    
+//    // 工具栏
+//    // 工具栏默认隐藏, 设置不隐藏
+//    [self.navigationController setToolbarHidden: NO animated: YES];
+//    
+////    self.navigationController.toolbar.translucent = NO;
+//    
+//    UIToolbarAppearance* toolAppearance = [[UIToolbarAppearance alloc] init];
+//    self.navigationController.toolbarHidden = NO;
+//    // 设置不透明
+//    [toolAppearance configureWithOpaqueBackground];
+//    
+//    toolAppearance.backgroundColor = [UIColor yellowColor];
+//    
+//    UIToolbar* toolBar = self.navigationController.toolbar;
+//    toolBar.standardAppearance = toolAppearance;
+//    toolBar.scrollEdgeAppearance = toolAppearance;
+//    toolBar.compactAppearance = toolAppearance;
+//    
+//
+//    
+////    toolBar.tintColor = [UIColor redColor];
+//    
+//    UIBarButtonItem* item1 = [[UIBarButtonItem alloc] initWithTitle: @"item1" style: UIBarButtonItemStylePlain target: self action: @selector(pressTap)];
+//    UIBarButtonItem* item2 = [[UIBarButtonItem alloc] initWithTitle: @"item2" style: UIBarButtonItemStylePlain target: self action: @selector(pressTap)];
+//    
+//    self.toolbarItems = @[item1, item2];
+//    [self.navigationController setToolbarHidden:NO animated:NO];
+//    
+////    toolappearance.backgroundColor = [UIColor ]
+//
+//    
+//}
+
+- (void)test01 {
     self.view.backgroundColor = [UIColor systemCyanColor];
     self.title = @"VCRoot";
-    
-    UIBarButtonItem* leftBtn = [[UIBarButtonItem alloc] initWithTitle: @"Btn" style: UIBarButtonItemStylePlain target: self action: @selector(pressTap)];
+
+    UIBarButtonItem* leftBtn = [[UIBarButtonItem alloc] initWithTitle:@"Btn"
+                                                                style:UIBarButtonItemStylePlain
+                                                               target:self
+                                                               action:@selector(pressTap)];
     self.navigationItem.leftBarButtonItem = leftBtn;
-    
-    // 设置不透明
-    // 默认是 YES, 透明
-    self.navigationController.navigationBar.translucent = YES;
-    
-    // 设置导航栏的背景颜色
-    self.navigationController.navigationBar.barTintColor = [UIColor redColor];
-    
-    UINavigationBarAppearance* appearance = [[UINavigationBarAppearance alloc] init];
-//    [appearance configureWithOpaqueBackground];
-    
-    appearance.backgroundColor = [UIColor greenColor];
-    
-    // 标题颜色
-    appearance.titleTextAttributes = @{
+
+    UINavigationBarAppearance* navAppearance = [[UINavigationBarAppearance alloc] init];
+    [navAppearance configureWithOpaqueBackground];
+    navAppearance.backgroundColor = [UIColor greenColor];
+    navAppearance.titleTextAttributes = @{
         NSForegroundColorAttributeName: [UIColor redColor]
     };
-    
-//    // 按钮颜色(按钮的文字颜色)
-//    appearance.buttonAppearance.normal.titleTextAttributes = @{
-//        NSForegroundColorAttributeName: [UIColor blueColor]
-//    };
-    
-    appearance.buttonAppearance.normal.titleTextAttributes = @{
+    navAppearance.buttonAppearance.normal.titleTextAttributes = @{
         NSForegroundColorAttributeName: [UIColor blackColor]
     };
-    
-    UINavigationBar* navbar = self.navigationController.navigationBar;
-    navbar.standardAppearance = appearance;
-    navbar.scrollEdgeAppearance = appearance;
-    
-    
-    
-    
-    // 工具栏
-    // 工具栏默认隐藏, 设置不隐藏
-    [self.navigationController setToolbarHidden: NO animated: YES];
-    
-//    self.navigationController.toolbar.translucent = NO;
-    
+
+    self.navigationController.navigationBar.standardAppearance = navAppearance;
+    self.navigationController.navigationBar.scrollEdgeAppearance = navAppearance;
+
     UIToolbarAppearance* toolAppearance = [[UIToolbarAppearance alloc] init];
-    self.navigationController.toolbarHidden = NO;
-    // 设置不透明
     [toolAppearance configureWithOpaqueBackground];
-    
     toolAppearance.backgroundColor = [UIColor yellowColor];
-    
+    toolAppearance.buttonAppearance.normal.titleTextAttributes = @{
+        NSForegroundColorAttributeName: [UIColor redColor]
+    };
+
     UIToolbar* toolBar = self.navigationController.toolbar;
     toolBar.standardAppearance = toolAppearance;
     toolBar.scrollEdgeAppearance = toolAppearance;
     toolBar.compactAppearance = toolAppearance;
-    
+    toolBar.tintColor = [UIColor redColor];
 
-    
-//    toolBar.tintColor = [UIColor redColor];
-    
-    UIBarButtonItem* item1 = [[UIBarButtonItem alloc] initWithTitle: @"item1" style: UIBarButtonItemStylePlain target: self action: @selector(pressTap)];
-    UIBarButtonItem* item2 = [[UIBarButtonItem alloc] initWithTitle: @"item2" style: UIBarButtonItemStylePlain target: self action: @selector(pressTap)];
-    
+    UIBarButtonItem* item1 = [[UIBarButtonItem alloc] initWithTitle:@"item1"
+                                                              style:UIBarButtonItemStylePlain
+                                                             target:self
+                                                             action:@selector(pressTap)];
+    UIBarButtonItem* item2 = [[UIBarButtonItem alloc] initWithTitle:@"item2"
+                                                              style:UIBarButtonItemStylePlain
+                                                             target:self
+                                                             action:@selector(pressTap)];
+
     self.toolbarItems = @[item1, item2];
     [self.navigationController setToolbarHidden:NO animated:NO];
-    
-//    toolappearance.backgroundColor = [UIColor ]
-
-    
 }
 - (void)viewDidLoad {
     [super viewDidLoad];
